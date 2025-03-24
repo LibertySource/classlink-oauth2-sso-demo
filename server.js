dcdfba4349ba66e6923ac671e88063f47d58bc8f
Jeff Holst <jholst@harriscomputer.com>
2025-03-24 10:57:04 -0500

    chore: cleanup code

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const fs = require('fs');
 const axios = require('axios');
 const app = express();
 
-const CLASSLINK_AUTH_URL = 'https://launchpad.classlink.com/oauth2/v2/auth';
+//const CLASSLINK_AUTH_URL = 'https://launchpad.classlink.com/oauth2/v2/auth';
 const CLASSLINK_TOKEN_URL = 'https://launchpad.classlink.com/oauth2/v2/token';
+const CLASSLINK_INFO_URL = 'https://nodeapi.classlink.com/v2/my/info';
 
 const PORT = process.env.PORT || 8080;
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -14,7 +15,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req, res) => {
-    const authUrl = `${CLASSLINK_AUTH_URL}?scope=profile&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+    //const authUrl = `${CLASSLINK_AUTH_URL}?scope=profile&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
 
     fs.readFile('index.html', 'utf8', (err, data) => {
         if (err) {
@@ -23,8 +24,8 @@ app.get('/', (req, res) => {
         }
         
         // Replace placeholder in HTML with authUrl
-        let html = data.replaceAll('{{authUrl}}', authUrl);
-        html = html.replaceAll('{{StudentName}}', process.env.STUDENT_NAME);
+        //let html = data.replaceAll('{{authUrl}}', authUrl);
+        let html = data.replaceAll('{{StudentName}}', process.env.STUDENT_NAME);
         html = html.replaceAll('{{StudentPassword}}', process.env.STUDENT_PASSWORD);
         html = html.replaceAll('{{TeacherName}}', process.env.TEACHER_NAME);
         html = html.replaceAll('{{TeacherPassword}}', process.env.TEACHER_PASSWORD);
@@ -32,18 +33,14 @@ app.get('/', (req, res) => {
         html = html.replaceAll('{{ParentPassword}}', process.env.PARENT_PASSWORD);
         html = html.replaceAll('{{AdminName}}', process.env.ADMIN_NAME);
         html = html.replaceAll('{{AdminPassword}}', process.env.ADMIN_PASSWORD);
+        html = html.replaceAll('[CLASSLINK_TOKEN_URL]', CLASSLINK_TOKEN_URL);
+        html = html.replaceAll('[CLASSLINK_INFO_URL]', CLASSLINK_INFO_URL);
+
         res.send(html);
     });
 });
 
-// Login page with ClassLink OAuth2 link
-app.get('/login', (req, res) => {
-    const authUrl = `${CLASSLINK_AUTH_URL}?scope=profile&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
-    res.redirect(authUrl);
-});
-
 // OAuth2 Callback route
-// app.get('/callback', async (req, res) => {
 app.get('/classlink_callback', async (req, res) => {
     const { code } = req.query;
     if (!code) {
@@ -63,20 +60,19 @@ app.get('/classlink_callback', async (req, res) => {
         
         const accessToken = response.data.access_token;
 
-       console.log(accessToken);
-        
-        const user = await axios.get('https://nodeapi.classlink.com/v2/my/info', {
+        const user = await axios.get(CLASSLINK_INFO_URL, {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
             },
         });
 
-        console.log(user.data)
-
         const resp = {
+            code: code,
             token: accessToken,
             user: user.data,
         };
+
+        console.log(resp);
 
         res.json(resp);
     } catch (error) {
